58e22ac9e7d85f675b2da504f8c9b0f080419afa
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-23 23:28:05 +0530

    react state

--- src/components/header.js
+++ src/components/header.js
@@ -8,6 +8,16 @@ import ReactDOM from 'react-dom';
 //class based component
 class Header extends Component{
 
+    constructor(props){
+        super(props);
+
+        //creating a state in react
+        //state is like a database
+        this.state={
+            keywords:'jon'
+        }
+    }
+
     inputChange(event){
         console.log(event.target.value)
     }
